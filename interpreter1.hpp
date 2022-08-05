8fe4cce2ab62049c22bd42b01353f4937e88071c
TikZaqaryan <110584877+TikZaqaryan@users.noreply.github.com>
2022-08-05 09:53:49 +0200

    Add files via upload

---
+++ interpreter1.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+
+
+void tip_(  std::string );
+void read ( std::string );
+void int_( std::string );
+void float_( std::string );
+void str_( std::string );
+void bool_( std::string );
+int var_name( char );
+void loop_( std::string );
+void acts( std::string );       
+void print( std::string );      
+void condition( std::string );
+bool compare( std::string, std::string, char );
+int mat_( int, int, char );
+
+std::map < char, int > imap;
+std::map < char, float > fmap;
+std::map < char, std::string > smap;
+std::map < char, std::string > bmap;
+
+
